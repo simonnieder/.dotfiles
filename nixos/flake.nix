885238ac7f915e8b11cd40831b98e7e4fd7fcee0
simonnieder <mail@simonnieder.com>
2026-05-20 20:29:03 +0200

    nix

---
+++ nixos/flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "Simon's NixOS config";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+  };
+
+  outputs = { self, nixpkgs, ... }: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+      ];
+    };
+  };
+}
